d73d1459ecdf67c07bffd53bc3a6187289fc7c8e
Christopher Arndt <chris@chrisarndt.de>
2015-05-29 01:52:09 +0200

    Normalize whitespace; add enum for params; add class attributes for raw octave, tuning and reso param values

--- src/mdaJX10.h
+++ src/mdaJX10.h
@@ -32,11 +32,38 @@
 #define TWOPI    6.2831853071795864f
 #define ANALOG   0.002f  //oscillator drift
 
+typedef enum {
+    OSC_MIX = 0,
+    OSC_TUNE = 1,
+    OSC_FINE = 2,
+    GLIDE = 3,
+    GLD_RATE = 4,
+    GLD_BEND = 5,
+    VCF_FREQ = 6,
+    VCF_RESO = 7,
+    VCF_ENV = 8,
+    VCF_LFO = 9,
+    VCF_VEL = 10,
+    VCF_ATT = 11,
+    VCF_DEC = 12,
+    VCF_SUS = 13,
+    VCF_REL = 14,
+    ENV_ATT = 15,
+    ENV_DEC = 16,
+    ENV_SUS = 17,
+    ENV_REL = 18,
+    LFO_RATE = 19,
+    VIBRATO = 20,
+    NOISE = 21,
+    OCTAVE = 22,
+    TUNING = 23
+} ParamIndex;
+
 class mdaJX10Program
 {
   friend class mdaJX10;
 public:
-	mdaJX10Program();
+  mdaJX10Program();
 private:
   float param[NPARAMS];
   char  name[24];
@@ -53,7 +80,7 @@ struct VOICE  //voice state
   float  sin1;
   float  sinx;
   float  dc;   //dc offset
-  
+
   float  detune;
   float  p2;    //sinc position
   float  pmax2; //loop length
@@ -62,13 +89,13 @@ struct VOICE  //voice state
   float  sin12;
   float  sinx2;
   float  dc2;   //dc offset
-  
+
   float  fc;  //filter cutoff root
   float  ff;  //filter cutoff
   float  f0;  //filter buffers
   float  f1;
   float  f2;
-  
+
   float  saw;
   //float  vca;  //current level  ///eliminate osc1 level when separate amp & filter envs?
   //float  env;  //envelope
@@ -80,7 +107,7 @@ struct VOICE  //voice state
   float  fenv;
   float  fenvd;
   float  fenvl;
-  
+
   float  lev;  //osc levels
   float  lev2;
   float  target; //period target
@@ -91,43 +118,43 @@ struct VOICE  //voice state
 class mdaJX10 : public AudioEffectX
 {
 public:
-	mdaJX10(audioMasterCallback audioMaster);
-	~mdaJX10();
+  mdaJX10(audioMasterCallback audioMaster);
+  ~mdaJX10();
 
-	virtual void processReplacing(float **inputs, float **outputs, int32_t sampleframes);
+  virtual void processReplacing(float **inputs, float **outputs, int32_t sampleframes);
 
-	virtual void setProgram(int32_t program);
-	virtual void setProgramName(char *name);
-	virtual void getProgramName(char *name);
-	virtual void setParameter(int32_t index, float value);
-	virtual float getParameter(int32_t index);
-	virtual void getParameterLabel(int32_t index, char *label);
-	virtual void getParameterDisplay(int32_t index, char *text);
-	virtual void getParameterName(int32_t index, char *text);
-	virtual void setSampleRate(float sampleRate);
-	virtual void setBlockSize(int32_t blockSize);
+  virtual void setProgram(int32_t program);
+  virtual void setProgramName(char *name);
+  virtual void getProgramName(char *name);
+  virtual void setParameter(int32_t index, float value);
+  virtual float getParameter(int32_t index);
+  virtual void getParameterLabel(int32_t index, char *label);
+  virtual void getParameterDisplay(int32_t index, char *text);
+  virtual void getParameterName(int32_t index, char *text);
+  virtual void setSampleRate(float sampleRate);
+  virtual void setBlockSize(int32_t blockSize);
   virtual void suspend();
   virtual void resume();
 
-	virtual bool getOutputProperties (int32_t index, LvzPinProperties* properties);
-	virtual bool getProgramNameIndexed (int32_t category, int32_t index, char* text);
-	virtual bool copyProgram (int32_t destination);
-	virtual bool getEffectName (char* name);
-	virtual bool getVendorString (char* text);
-	virtual bool getProductString (char* text);
-	virtual int32_t getVendorVersion () {return 1;}
-	virtual int32_t canDo (const char* text);
+  virtual bool getOutputProperties (int32_t index, LvzPinProperties* properties);
+  virtual bool getProgramNameIndexed (int32_t category, int32_t index, char* text);
+  virtual bool copyProgram (int32_t destination);
+  virtual bool getEffectName (char* name);
+  virtual bool getVendorString (char* text);
+  virtual bool getProductString (char* text);
+  virtual int32_t getVendorVersion () {return 1;}
+  virtual int32_t canDo (const char* text);
 
 private:
-	int32_t processEvent(const LV2_Atom_Event* ev);
-	void update();  //my parameter update
+  int32_t processEvent(const LV2_Atom_Event* ev);
+  void update();  //my parameter update
   void noteOn(int32_t note, int32_t velocity);
   void fillpatch(int32_t p, const char *name,
-                 float p0,  float p1,  float p2,  float p3,  float p4,  float p5, 
+                 float p0,  float p1,  float p2,  float p3,  float p4,  float p5,
                  float p6,  float p7,  float p8,  float p9,  float p10, float p11,
-                 float p12, float p13, float p14, float p15, float p16, float p17, 
+                 float p12, float p13, float p14, float p15, float p16, float p17,
                  float p18, float p19, float p20, float p21, float p22, float p23);
-  
+
   mdaJX10Program* programs;
   float Fs;
 
@@ -138,9 +165,9 @@ private:
   int32_t sustain, activevoices;
   VOICE voice[NVOICES];
 
-  float semi, cent;
-  float tune, detune;
-  float filtf, fzip, filtq, filtlfo, filtenv, filtvel, filtwhl;
+  float semi, cent, octave;
+  float tuning, tune, detune;
+  float filtf, fzip, filtq, filtlfo, filtenv, filtvel, filtwhl, reso;
   float oscmix, noisemix;
   float att, dec, sus, rel, fatt, fdec, fsus, frel;
   float lfo, dlfo, modwhl, press, pbend, ipbend, rezwhl;
